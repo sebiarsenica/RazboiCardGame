e52844acea0fc416b8dbc2d230b6c5ca9c147fd6
victoria <victoriabuga64@gmail.com>
2023-05-07 02:26:20 +0300

    regulile adaugate

--- RazboiCardGame/MainPage.Designer.cs
+++ RazboiCardGame/MainPage.Designer.cs
@@ -30,18 +30,25 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainPage));
             panelMenu = new Panel();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
             label2 = new Label();
             linkLabel1 = new LinkLabel();
             label1 = new Label();
-            label3 = new Label();
             panelMenu.SuspendLayout();
             SuspendLayout();
             // 
             // panelMenu
             // 
             panelMenu.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            panelMenu.AutoSize = true;
             panelMenu.BackgroundImage = (Image)resources.GetObject("panelMenu.BackgroundImage");
             panelMenu.BackgroundImageLayout = ImageLayout.None;
+            panelMenu.Controls.Add(label6);
+            panelMenu.Controls.Add(label5);
+            panelMenu.Controls.Add(label4);
             panelMenu.Controls.Add(label3);
             panelMenu.Controls.Add(label2);
             panelMenu.Controls.Add(linkLabel1);
@@ -52,13 +59,66 @@
             panelMenu.TabIndex = 0;
             panelMenu.Paint += panelMenu_Paint;
             // 
+            // label6
+            // 
+            label6.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            label6.AutoSize = true;
+            label6.BackColor = Color.Transparent;
+            label6.Font = new Font("ArcadeClassic", 23.9999981F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.ForeColor = Color.White;
+            label6.Location = new Point(691, 443);
+            label6.Name = "label6";
+            label6.Size = new Size(604, 64);
+            label6.TabIndex = 7;
+            label6.Text = "4. JUCATORUL CARE RAMANE FARA CARTI \r\nPIERDE JOCUL";
+            // 
+            // label5
+            // 
+            label5.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            label5.AutoSize = true;
+            label5.BackColor = Color.Transparent;
+            label5.Font = new Font("ArcadeClassic", 23.9999981F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.ForeColor = Color.White;
+            label5.Location = new Point(691, 370);
+            label5.Name = "label5";
+            label5.Size = new Size(652, 64);
+            label5.TabIndex = 6;
+            label5.Text = "3.  JOCUL SE TERMINA IN MOMENTUL IN CARE\r\n UNUL DIN JUCATORI RAMANE FARA CARTI ";
+            // 
+            // label4
+            // 
+            label4.Anchor = AnchorStyles.Right;
+            label4.AutoSize = true;
+            label4.BackColor = Color.Transparent;
+            label4.Font = new Font("ArcadeClassic", 23.9999981F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.ForeColor = Color.White;
+            label4.Location = new Point(691, 294);
+            label4.Name = "label4";
+            label4.Size = new Size(633, 64);
+            label4.TabIndex = 5;
+            label4.Text = "2.  JUCATORUL CARE ARE CARTEA MAI MARE \r\nCASTIGA RUNDA";
+            // 
+            // label3
+            // 
+            label3.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.Font = new Font("ArcadeClassic", 23.9999981F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.ForeColor = Color.White;
+            label3.Location = new Point(691, 215);
+            label3.Name = "label3";
+            label3.Size = new Size(671, 64);
+            label3.TabIndex = 4;
+            label3.Text = "1.  FIECARE JUCATOR O SA APESE PE BUTONUL\r\n \"DRAW\" PENTRU A GENERA O CARTE";
+            // 
             // label2
             // 
+            label2.Anchor = AnchorStyles.Top | AnchorStyles.Right;
             label2.AutoSize = true;
             label2.BackColor = Color.Transparent;
             label2.Font = new Font("ArcadeClassic", 47.9999962F, FontStyle.Regular, GraphicsUnit.Point);
             label2.ForeColor = Color.White;
-            label2.Location = new Point(952, 184);
+            label2.Location = new Point(835, 151);
             label2.Name = "label2";
             label2.Size = new Size(237, 64);
             label2.TabIndex = 3;
@@ -68,6 +128,7 @@
             // linkLabel1
             // 
             linkLabel1.ActiveLinkColor = Color.White;
+            linkLabel1.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
             linkLabel1.AutoSize = true;
             linkLabel1.BackColor = Color.Transparent;
             linkLabel1.BorderStyle = BorderStyle.FixedSingle;
@@ -93,33 +154,25 @@
             label1.BackColor = Color.Transparent;
             label1.Font = new Font("ArcadeClassic", 92.25F, FontStyle.Regular, GraphicsUnit.Point);
             label1.ForeColor = Color.White;
-            label1.Location = new Point(0, 136);
+            label1.Location = new Point(23, 125);
             label1.Name = "label1";
             label1.Size = new Size(460, 123);
             label1.TabIndex = 0;
             label1.Text = "Razboi";
             label1.Click += label1_Click;
             // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.BackColor = Color.Transparent;
-            label3.Font = new Font("ArcadeClassic", 35.9999962F, FontStyle.Bold, GraphicsUnit.Point);
-            label3.Location = new Point(944, 255);
-            label3.Name = "label3";
-            label3.Size = new Size(182, 48);
-            label3.TabIndex = 4;
-            label3.Text = "label3";
-            // 
             // MainPage
             // 
             ClientSize = new Size(1365, 635);
             Controls.Add(panelMenu);
+            FormBorderStyle = FormBorderStyle.SizableToolWindow;
             MinimumSize = new Size(1365, 635);
             Name = "MainPage";
+            Sizable = false;
             panelMenu.ResumeLayout(false);
             panelMenu.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -131,5 +184,8 @@
         private LinkLabel linkLabel1;
         private Label label2;
         private Label label3;
+        private Label label4;
+        private Label label6;
+        private Label label5;
     }
 }
